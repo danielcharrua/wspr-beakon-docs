5c268a1b7fc34e8fb0ec8b4b90dfff5bcfd3796c
Daniel Pereyra Costas <danielp@charrua.es>
2025-11-03 18:16:09 +0100

    fix: gh-pages fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,16 +20,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://wspr-beacon-docs.github.io',
+  url: 'https://danielcharrua.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/wspr-beakon-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'danielcharrua', // Usually your GitHub org/user name.
   projectName: 'wspr-beakon-docs', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages', // The branch to deploy to
+  
   onBrokenLinks: 'throw',
 
   trailingSlash: false,
